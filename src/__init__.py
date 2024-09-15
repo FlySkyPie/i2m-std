3ccece55af966a424c4f59ab63f7e12795d0a28f
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2024-09-15 17:05:15 +0800

    refactor: Move class to function

--- src/__init__.py
+++ src/__init__.py
@@ -38,6 +38,7 @@ from bpy.props import (
 from .clearslot import KeI2Mclearslot
 from .batchbrowser import KeI2Mbatchbrowser
 from .filebrowser import KeI2Mfilebrowser
+from .reload import KeI2Mreload
 
 from .utilities import load_slot
 
@@ -1285,39 +1286,6 @@ class KeI2M(Operator):
         if self.geo != "BOOLEAN" and self.front_only:
             self.front_only = False
 
-        return {"FINISHED"}
-
-
-class KeI2Mreload(Operator):
-    bl_idname = "ke.i2m_reload"
-    bl_label = "I2M Reload"
-    bl_description = "Reload I2M image(s)"
-
-    def execute(self, context):
-        k = context.scene.kei2m
-        front_img = bpy.data.images.get(k.FRONT)
-        right_img = bpy.data.images.get(k.RIGHT)
-        top_img = bpy.data.images.get(k.TOP)
-        back_img = bpy.data.images.get(k.BACK)
-        left_img = bpy.data.images.get(k.LEFT)
-        bottom_img = bpy.data.images.get(k.BOTTOM)
-        images = [front_img, right_img, top_img, back_img, left_img, bottom_img]
-        kindex = ["FRONT", "RIGHT", "TOP", "BACK", "LEFT", "BOTTOM"]
-        missing = 0
-        found = 0
-        for index, i in enumerate(images):
-            if i is None:
-                k[kindex[index]] = ""
-                missing += 1
-            if i is not None:
-                i.reload()
-        context.area.tag_redraw()
-        if missing:
-            self.report(
-                {"INFO"},
-                "%s slot(s) reloaded - %s slot(s) empty/invalid"
-                % (str(found), str(missing)),
-            )
         return {"FINISHED"}
 
 
---
+++ src/reload.py
@@ -0,0 +1,34 @@
+import bpy
+from bpy.types import PropertyGroup, Operator, Panel, AddonPreferences
+
+class KeI2Mreload(Operator):
+    bl_idname = "ke.i2m_reload"
+    bl_label = "I2M Reload"
+    bl_description = "Reload I2M image(s)"
+
+    def execute(self, context):
+        k = context.scene.kei2m
+        front_img = bpy.data.images.get(k.FRONT)
+        right_img = bpy.data.images.get(k.RIGHT)
+        top_img = bpy.data.images.get(k.TOP)
+        back_img = bpy.data.images.get(k.BACK)
+        left_img = bpy.data.images.get(k.LEFT)
+        bottom_img = bpy.data.images.get(k.BOTTOM)
+        images = [front_img, right_img, top_img, back_img, left_img, bottom_img]
+        kindex = ["FRONT", "RIGHT", "TOP", "BACK", "LEFT", "BOTTOM"]
+        missing = 0
+        found = 0
+        for index, i in enumerate(images):
+            if i is None:
+                k[kindex[index]] = ""
+                missing += 1
+            if i is not None:
+                i.reload()
+        context.area.tag_redraw()
+        if missing:
+            self.report(
+                {"INFO"},
+                "%s slot(s) reloaded - %s slot(s) empty/invalid"
+                % (str(found), str(missing)),
+            )
+        return {"FINISHED"}
