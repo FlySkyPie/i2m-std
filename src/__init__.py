0d3958bf3b670b956a6bcd1d3c619067c4acb027
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2024-09-15 17:32:16 +0800

    refactor: Move a class to separate file

--- src/__init__.py
+++ src/__init__.py
@@ -42,6 +42,7 @@ from .reload import KeI2Mreload
 from .main import KeI2M
 from .ui import VIEW3D_PT_i2m
 from .prefs.addonprefs import KeI2Maddonprefs
+from .prefs.props import KeI2Mprops
 
 from .utilities import load_slot
 
@@ -109,81 +110,6 @@ def reduce_colors(pixelmap, threshold=0.51, cap=None):
             if p[2] in g:
                 p[2] = g[0][0]
     return pixelmap, [g[0][0] for g in color_groups]
-
-
-# ------------------------------------------------------------------------------------------------------------
-# Prefs
-# ------------------------------------------------------------------------------------------------------------
-
-
-
-class KeI2Mprops(PropertyGroup):
-    FRONT: StringProperty(default="", name="Front Image", description="-Y Axis Image")
-    RIGHT: StringProperty(default="", name="Right Image", description="+X Axis Image")
-    TOP: StringProperty(default="", name="Top Image", description="+Z Axis Image")
-    BACK: StringProperty(default="", name="Back Image", description="+Y Axis Image")
-    LEFT: StringProperty(default="", name="Left Image", description="-X Axis Image")
-    BOTTOM: StringProperty(default="", name="Bottom Image", description="-Z Axis Image")
-    autofill: BoolProperty(
-        name="Autofill",
-        default=False,
-        description="Auto-Fill other slots by suffixes when loading an image.\n "
-        "Valid suffixes:\n"
-        "_front, _right, _top\n"
-        "_back,  _left,  _bottom",
-    )
-    info: BoolProperty(
-        name="kei2m General Info",
-        default=False,
-        description="Load Alpha Images to convert into mesh (in Object Mode).\n"
-        "A single image is required for PLANE & SCREW (& C2M) modes.\n"
-        "2 or more images are required for BOOLEAN mode.\n"
-        "Adjust result in Redo Panel.\n"
-        "Non-alpha color option in Addon Preferences.\n"
-        "See progress output in Console Window.",
-    )
-    opacity: IntProperty(default=95)
-    workres: StringProperty(default="128")
-    geo: EnumProperty(
-        items=[
-            ("PLANE", "Plane", "Converts into a Plane Mesh", "", 1),
-            (
-                "SCREW",
-                "Screw",
-                "A Screw Modifier revolves one side of a (symmetrical) image to a cylindrical shape",
-                "",
-                2,
-            ),
-            (
-                "BOOLEAN",
-                "Boolean",
-                "Uses 2-3 images (front + right/top) to carve out a rough 3d shape using Intersect",
-                "",
-                3,
-            ),
-            (
-                "C2M",
-                "Color 2 Material",
-                "Splits along color borders and assigns color materials on an mesh plane",
-                "",
-                4,
-            ),
-        ],
-        name="Geo Type",
-        default="PLANE",
-        description="Which type of geo conversion to use",
-    )
-    screw_flip: BoolProperty(default=False)
-    screw_xcomp: IntProperty(default=15)
-    reduce: StringProperty(default="SIMPLE")
-    shade_smooth: BoolProperty(default=True)
-    front_only: BoolProperty(default=False)
-    qnd_mat: BoolProperty(default=False)
-    apply: BoolProperty(default=False)
-    apply_none: BoolProperty(default=False)
-    angle: FloatProperty(default=0.5)
-    custom_workres: IntProperty(default=0)
-    vcolor: BoolProperty(default=False)
 
 
 # ------------------------------------------------------------------------------------------------------------
---
+++ src/prefs/props.py
@@ -0,0 +1,79 @@
+from bpy.types import PropertyGroup, Operator, Panel, AddonPreferences
+from bpy.props import (
+    BoolProperty,
+    PointerProperty,
+    FloatProperty,
+    StringProperty,
+    EnumProperty,
+    IntProperty,
+    FloatVectorProperty,
+)
+
+
+class KeI2Mprops(PropertyGroup):
+    FRONT: StringProperty(default="", name="Front Image", description="-Y Axis Image")
+    RIGHT: StringProperty(default="", name="Right Image", description="+X Axis Image")
+    TOP: StringProperty(default="", name="Top Image", description="+Z Axis Image")
+    BACK: StringProperty(default="", name="Back Image", description="+Y Axis Image")
+    LEFT: StringProperty(default="", name="Left Image", description="-X Axis Image")
+    BOTTOM: StringProperty(default="", name="Bottom Image", description="-Z Axis Image")
+    autofill: BoolProperty(
+        name="Autofill",
+        default=False,
+        description="Auto-Fill other slots by suffixes when loading an image.\n "
+        "Valid suffixes:\n"
+        "_front, _right, _top\n"
+        "_back,  _left,  _bottom",
+    )
+    info: BoolProperty(
+        name="kei2m General Info",
+        default=False,
+        description="Load Alpha Images to convert into mesh (in Object Mode).\n"
+        "A single image is required for PLANE & SCREW (& C2M) modes.\n"
+        "2 or more images are required for BOOLEAN mode.\n"
+        "Adjust result in Redo Panel.\n"
+        "Non-alpha color option in Addon Preferences.\n"
+        "See progress output in Console Window.",
+    )
+    opacity: IntProperty(default=95)
+    workres: StringProperty(default="128")
+    geo: EnumProperty(
+        items=[
+            ("PLANE", "Plane", "Converts into a Plane Mesh", "", 1),
+            (
+                "SCREW",
+                "Screw",
+                "A Screw Modifier revolves one side of a (symmetrical) image to a cylindrical shape",
+                "",
+                2,
+            ),
+            (
+                "BOOLEAN",
+                "Boolean",
+                "Uses 2-3 images (front + right/top) to carve out a rough 3d shape using Intersect",
+                "",
+                3,
+            ),
+            (
+                "C2M",
+                "Color 2 Material",
+                "Splits along color borders and assigns color materials on an mesh plane",
+                "",
+                4,
+            ),
+        ],
+        name="Geo Type",
+        default="PLANE",
+        description="Which type of geo conversion to use",
+    )
+    screw_flip: BoolProperty(default=False)
+    screw_xcomp: IntProperty(default=15)
+    reduce: StringProperty(default="SIMPLE")
+    shade_smooth: BoolProperty(default=True)
+    front_only: BoolProperty(default=False)
+    qnd_mat: BoolProperty(default=False)
+    apply: BoolProperty(default=False)
+    apply_none: BoolProperty(default=False)
+    angle: FloatProperty(default=0.5)
+    custom_workres: IntProperty(default=0)
+    vcolor: BoolProperty(default=False)
